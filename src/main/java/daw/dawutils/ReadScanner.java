01a52d4a74366c558fb6681d6e65e1de8d5546c3
Andyloz <lozandyloz@gmail.com>
2020-02-04 12:20:42 +0100

    initial commit

---
+++ src/main/java/daw/dawutils/ReadScanner.java
@@ -0,0 +1,106 @@
+package daw.dawutils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author andyloz
+ */
+public class ReadScanner {
+    public static int ReadInt() {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        
+        System.out.print("Introduzca un número entero: ");
+        
+        boolean repeat;        
+        do {
+            repeat = false;
+            try {
+                number = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.print("Introduzca un valor válido: ");
+                repeat = true;
+            } finally {
+                sc.nextLine();
+            }
+        } while (repeat);
+        
+        return number;
+    }
+    
+    public static int ReadInt(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        
+        System.out.print(prompt);
+        
+        boolean repeat;        
+        do {
+            repeat = false;
+            try {
+                number = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.print("Introduzca un valor válido: ");
+                repeat = true;
+            } finally {
+                sc.nextLine();
+            }
+        } while (repeat);
+        
+        return number;
+    }
+    
+    public static int ReadInt(int start, int end) {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        
+        System.out.print("Introduzca un número entero: ");
+        
+        boolean repeat;        
+        do {
+            repeat = false;
+            try {
+                number = sc.nextInt();
+                
+                if (number < start || number > end) {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Introduzca un valor válido: ");
+                repeat = true;
+            } finally {
+                sc.nextLine();
+            }
+        } while (repeat);
+        
+        return number;
+    }
+    
+    public static int ReadInt(String prompt, int start, int end) {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        
+        System.out.println(prompt);
+        
+        boolean repeat;        
+        do {
+            repeat = false;
+            try {
+                number = sc.nextInt();
+                
+                if (number < start || number > end) {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Introduzca un valor válido: ");
+                repeat = true;
+            } finally {
+                sc.nextLine();
+            }
+        } while (repeat);
+        
+        return number;
+    }
+}
